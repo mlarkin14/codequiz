6d08f3d51999f232d205d94c56c426e2dde2c64a
Matt Larkin <mlarkin14@gmail.com>
2022-04-03 14:51:17 -0400

    added js and css to make quiz functional

---
+++ Assets/js/questions.js
@@ -0,0 +1,57 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "Commonly used data types DO NOT include:",
+        answer: "Alerts",
+        options: [
+            "Strings",
+            "Booleans",
+            "Alerts",
+            "Numbers"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The condition in an if / else statement is enclosed with _______.",
+        answer: "Parenthesis",
+        options: [
+            "Quotes",
+            "Curly brackets",
+            "Parenthesis",
+            "Square brackets"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Arrays in JavaScript can be used to store _______.",
+        answer: "All of the above",
+        options: [
+            "Numbers and strings",
+            "Other arrays",
+            "Booleans",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 4,
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answer: "Quotes",
+        options: [
+            "Commas",
+            "Curly brackets",
+            "Quotes",
+            "Parenthesis"
+        ]
+    },
+    {
+        numb: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "Console.log",
+        options: [
+            "JavaScript",
+            "Terminal/Bash",
+            "For loops",
+            "Console.log"
+        ]
+    }
+];
